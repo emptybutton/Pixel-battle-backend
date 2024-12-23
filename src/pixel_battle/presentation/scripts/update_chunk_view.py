42a5141444b9eb8be27e070bd0a4d79052fc4a75
Alexander Smolin <88573504+emptybutton@users.noreply.github.com>
2024-12-23 20:20:02 +0700

    feat(`presentation`): add `UpdateChunkView` script

---
+++ src/pixel_battle/presentation/scripts/update_chunk_view.py
@@ -0,0 +1,74 @@
+import sys
+from dataclasses import dataclass
+from datetime import datetime
+from io import TextIOBase
+from typing import Any, cast
+
+from pixel_battle.application.interactors.update_chunk_view import (
+    UpdateChunkView as Interactor,
+)
+from pixel_battle.application.ports.chunk_view import (
+    ChunkView,
+)
+
+
+@dataclass(kw_only=True, frozen=True, slots=True)
+class UpdateChunkView:
+    interactor: Interactor[ChunkView, Any]
+    ok_file: TextIOBase = cast(TextIOBase, sys.stdout)
+    error_file: TextIOBase = cast(TextIOBase, sys.stderr)
+
+    async def __call__(self) -> None:
+        if self.__is_help():
+            self.__print_help()
+
+        args = self.__parse_args()
+
+        if args is None:
+            return
+
+        chunk_number_x, chunk_number_y = args
+
+        self.__print("Updating...")
+        start_time = datetime.now()
+
+        await self.interactor(chunk_number_x, chunk_number_y)
+
+        end_time = datetime.now()
+        time_delta = end_time - start_time
+
+        self.__print(f"The chunk view was updated. (Finished in {time_delta})")
+
+    def __is_help(self) -> bool:
+        return len(sys.argv) == 2 and sys.argv[1] == "--help"  # noqa: PLR2004
+
+    def __parse_args(self) -> tuple[int, int] | None:
+        if len(sys.argv) != 3:  # noqa: PLR2004
+            self.__print_error()
+            return None
+
+        try:
+            chunk_number_x, chunk_number_y = int(sys.argv[1]), int(sys.argv[2])
+        except ValueError:
+            self.__print_error()
+            return None
+
+        if chunk_number_x not in range(10) or chunk_number_y not in range(10):
+            self.__print_error()
+            return None
+
+        return chunk_number_x, chunk_number_y
+
+    def __print_error(self) -> None:
+        text = "Coordinates must be numbers in range(0, 10)."
+        self.__print(text, panic=True)
+
+    def __print_help(self) -> None:
+        text = (
+            "\nUsage:  python [script_path] [chunk_number_x] [chunk_number_y]"
+            "\n\nCoordinates must be numbers in range(0, 10)."
+        )
+        self.__print(text)
+
+    def __print(self, text: str, *, panic: bool = False) -> None:
+        print(text, file=self.error_file if panic else self.ok_file)
---
+++ tests/test_pixel_battle/test_presentation/test_scripts/test_update_chunk_view.py
@@ -0,0 +1,72 @@
+from io import StringIO
+from sys import argv
+
+from pytest import fixture, mark
+
+from pixel_battle.application.interactors.update_chunk_view import (
+    UpdateChunkView as Interactor,
+)
+from pixel_battle.infrastructure.adapters.broker import InMemoryBroker
+from pixel_battle.infrastructure.adapters.chunk_view import (
+    DefaultCollectionChunkViewOf,
+)
+from pixel_battle.infrastructure.adapters.chunk_views import InMemoryChunkViews
+from pixel_battle.infrastructure.adapters.lock import FakeLock
+from pixel_battle.infrastructure.adapters.offsets import InMemoryOffsets
+from pixel_battle.presentation.scripts.update_chunk_view import (
+    UpdateChunkView as Script,
+)
+
+
+@fixture
+def script() -> Script:
+    ok_file = StringIO()
+    error_file = StringIO()
+
+    interactor = Interactor(
+        broker=InMemoryBroker(),
+        lock=FakeLock(),
+        default_chunk_view_of=DefaultCollectionChunkViewOf(),
+        chunk_views=InMemoryChunkViews(),
+        offsets_of_latest_compressed_events=InMemoryOffsets(),
+    )
+
+    return Script(ok_file=ok_file, error_file=error_file, interactor=interactor)
+
+
+async def test_ok(script: Script) -> None:
+    argv[:] = ["_", "0", "0"]
+
+    await script()
+
+    output = script.ok_file.getvalue().split("\n")
+
+    assert output[0] == "Updating..."
+    assert output[1].startswith("The chunk view was updated. (")
+    assert output[1].endswith(")")
+
+
+@mark.parametrize(
+    "args", (["0", "99"], ["-1", "0"], ["X", "0"], ["9"], ["--helps"])
+)
+async def test_with_invalid_args(script: Script, args: list[str]) -> None:
+    argv[:] = ["_", *args]
+
+    await script()
+
+    output = script.error_file.getvalue()
+    assert output == "Coordinates must be numbers in range(0, 10).\n"
+
+
+async def test_help(script: Script) -> None:
+    argv[:] = ["_", "--help"]
+
+    await script()
+
+    output = script.ok_file.getvalue()
+    excepted_output = (
+        "\nUsage:  python [script_path] [chunk_number_x] [chunk_number_y]"
+        "\n\nCoordinates must be numbers in range(0, 10).\n"
+    )
+
+    assert output == excepted_output
