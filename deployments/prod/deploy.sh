74f2f48140bf43077ba9ec382f848e7864257920
Alexander Smolin <88573504+emptybutton@users.noreply.github.com>
2025-08-23 20:57:56 +0700

    chore(`prod`): merge `deploy` and `redeploy`

--- deployments/prod/deploy.sh
+++ deployments/prod/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-source deployments/prod/conf.sh
+timeout=900 # 15m
+conf=deployments/prod/docker-compose.yaml
+
+docker compose -f $conf down --wait-timeout $timeout
+docker compose -f $conf build
 
 docker compose -f $conf up redis1 redis2 redis3 -d --wait --wait-timeout $timeout
 docker exec pixel-battle-redis1 ash /mnt/scripts/create-cluster.sh
